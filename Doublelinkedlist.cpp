ff4af7f12518ed97108da87d3f1312e62eeb2af9
raffibromabar-web <raffibromabar@gmail.com>
2026-05-01 16:17:15 +0700

    memambahkan fitur untuk memasukkan data

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -20,4 +20,26 @@ public:
     {
         START = NULL;
     }
+
+    void addNode()
+    {
+        int nim;
+        cout << "Enter the roll number of the student: ";
+        cin >> nim;
+
+        // Step 1: Allocate memory for new node
+        Node *newnode = new Node();
+
+        // Step 2: Assign value to the data field
+        newnode->noMhs = nim;
+
+        // If list is empty or inserting at 1st is empty or not in sorted
+        if (START == NULL || nim <= START->noMhs)
+        {
+            if (START != NULL && nim == START->noMhs)
+            {
+                cout << "\nDuplicate roll numbers not allowed" << endl;
+                return;
+            }
+    
 };
